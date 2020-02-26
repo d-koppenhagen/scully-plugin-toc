b7617d5434503c8fdaf548a9f41335db0ec982e4
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-02-26 22:53:17 +0100

    refactor: split index and functions
    necessary to not call `registerPlugin` directly when importing in tests

--- tests/toc.spec.ts
+++ tests/toc.spec.ts
@@ -1,5 +1,6 @@
-import { headingLevel } from '../src';
+import { headingLevel } from '../src/toc';
 
 test('should return the heading level number', () => {
   expect(headingLevel('h1')).toBe(1);
+  expect(headingLevel('h2')).toBe(2);
 });
