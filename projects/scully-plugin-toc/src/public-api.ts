14228c3a80081f814262463b5337ad31dff4dad5
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-05-23 18:56:07 +0200

    feat: export `TocPluginName` and `TocConfig`

--- projects/scully-plugin-toc/src/public-api.ts
+++ projects/scully-plugin-toc/src/public-api.ts
@@ -2,3 +2,5 @@
  * Public API Surface of scully-plugin-toc
  */
 export { getTocPlugin } from './lib/index';
+export { TocConfig } from './lib/interfaces';
+export { TocPluginName } from './lib/constants';
