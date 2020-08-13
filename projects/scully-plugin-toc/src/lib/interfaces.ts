570ed3b03dcc07c64ed894004b00c050596da481
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-08-13 19:14:02 +0200

    refactor: make props optional

--- projects/scully-plugin-toc/src/lib/interfaces.ts
+++ projects/scully-plugin-toc/src/lib/interfaces.ts
@@ -1,7 +1,7 @@
 export type Level = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
 export interface TocConfig {
-  blogAreaSelector: string;
-  insertSelector: string;
-  level: Level[];
+  blogAreaSelector?: string;
+  insertSelector?: string;
+  level?: Level[];
 }
